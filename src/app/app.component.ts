ecd8b2b4727375ca09a3509e04512b7fc1c507f1
ACER <carlos2000ortiz29@gmail.com>
2022-08-15 17:31:36 -0500

    API + AngularMaterial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+/* Se importa las clases(service) para usar sus edpoints */
+import { CharactersService } from './services/characters.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'API-TheRickandMorty';
+  personajes: any = {}; // creamos esta variable vacia
+
+  constructor(private serviceCharacters: CharactersService){}
+
+  ngOnInit(): void {
+    this.serviceCharacters.getAllCharacters().subscribe(data =>{
+      // results nos trae la info de la api
+      /* SE MIRA EN EL NAVEGADOR EN LA PARTE DE Network - character - Preview.
+        esta dos objetos, uno con la información de la peticion 
+        y el otro con los personajes
+      */
+      this.personajes = data.results;
+      console.log(this.personajes);
+    },
+    error => console.log(error)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
+/* 
+  AQUI SE IMPORTA TODOS LOS ELEMENTOS(librerias, componentes, service, etc) 
+  QUE SE USARA EN EL PROYECTO 
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* ANGULAR MATERIAL */
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
+/* COMPONENTES */
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+/* SERVICES */
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
